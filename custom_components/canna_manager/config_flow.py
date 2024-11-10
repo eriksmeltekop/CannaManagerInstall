111f675d807588460f81b636ec8b273b63b0a733
eriksmeltekop <50347956+eriksmeltekop@users.noreply.github.com>
2024-11-10 12:42:05 -0500

    Add files via upload

--- custom_components/canna_manager/config_flow.py
+++ custom_components/canna_manager/config_flow.py
@@ -29,3 +29,23 @@ class CannabisGrowManagerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 }
             ),
         )
+import voluptuous as vol
+
+class CannabisGrowManagerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="Cannabis Grow Manager", data=user_input)
+
+        # Initial user form for irrigation setup
+        irrigation_schema = vol.Schema({
+            vol.Required("irrigation_zone_1_start"): str,
+            vol.Required("irrigation_zone_1_duration"): int,
+            vol.Required("lighting_schedule_start"): str,
+            vol.Required("lighting_schedule_end"): str,
+            vol.Optional("temperature_target", default=75): int,
+            vol.Optional("humidity_target", default=60): int,
+        })
+
+        return self.async_show_form(step_id="user", data_schema=irrigation_schema)
